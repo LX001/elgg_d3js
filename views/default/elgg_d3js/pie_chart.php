d7b95a82368d8ebe66cb6b196ce14fe6c4038ac8
LX001 <alexandre.robert1989@gmail.com>
2014-09-01 17:21:02 +0200

    Double pie chart v2
    It's working !

--- views/default/elgg_d3js/pie_chart.php
+++ views/default/elgg_d3js/pie_chart.php
@@ -18,8 +18,7 @@ $content = '	<div id="' . $id . '"></div>
   stroke: #fff;
 }
 	#' . $id . ' {
-		top: 50px;
-		left: 100px;
+			float:left;
 	}
 	
 
@@ -33,7 +32,7 @@ var width = 500,
 var color = d3.scale.ordinal()
     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-var arc' . $elgg_d3js_unique_id . '= d3.svg.arc()
+var arc= d3.svg.arc()
     .outerRadius(radius - 10)
     .innerRadius(0);
 
@@ -41,7 +40,7 @@ var pie = d3.layout.pie()
     .sort(null)
     .value(function(d) { return d.size; });
 
-var svg = d3.select("#' . $id . '").append("svg")
+var svg' . $elgg_d3js_unique_id . ' = d3.select("#' . $id . '").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
@@ -53,17 +52,17 @@ d3.json("' . $dataurl . '", function(error, data) {
     d.size = +d.size;
   });
 
-  var g = svg.selectAll(".arc' . $elgg_d3js_unique_id . '")
+  var g = svg' . $elgg_d3js_unique_id . '.selectAll(".arc")
       .data(pie(data))
     .enter().append("g")
-      .attr("class", "arc' . $elgg_d3js_unique_id . '");
+      .attr("class", "arc");
 
   g.append("path")
-      .attr("d", arc' . $elgg_d3js_unique_id . ')
+      .attr("d", arc)
       .style("fill", function(d) { return color(d.data.name); });
 
   g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc' . $elgg_d3js_unique_id . '.centroid(d) + ")"; })
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
       .attr("dy", ".35em")
       .style("text-anchor", "middle")
       .text(function(d) { return d.data.name; });
