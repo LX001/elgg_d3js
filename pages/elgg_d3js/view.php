ecfaf9ba382456bab8bfd2f4e02d93915281344e
Facyla ~ Florian DANIEL <facyla@gmail.com>
2014-09-08 16:07:53 +0200

    README + view structure

--- pages/elgg_d3js/view.php
+++ pages/elgg_d3js/view.php
@@ -7,38 +7,46 @@
  global $CONFIG;
 $viztype = get_input('viztype',''); 
 
+// Set data root URLs
 $data_url = $CONFIG->url . 'mod/elgg_d3js/data/';
 $dataurl = $CONFIG->url . 'd3js/data/' . $viztype;
 
 elgg_push_breadcrumb(elgg_echo('elgg_d3js'), '/d3js');
 if (!empty($viztype)) elgg_push_breadcrumb(elgg_echo($viztype));
 
+// Load D3 lib
 elgg_load_js('elgg:elgg_d3js');
 
-//call a view corresponding to a vizualisation
-switch($viztype)
-{
+
+// Call a view corresponding to a visualisation
+switch($viztype) {
 	case 'd3js_cfl' :
 		$content = elgg_view('elgg_d3js/collapsible_force_layout',array('dataurl' => $dataurl));
 		break;
+	
 	case 'd3js_bubble' :
 		$content = elgg_view('elgg_d3js/bubble_chart',array('dataurl' => $dataurl));
 		break;
+	
 	case 'd3js_circle' :
 		$content = elgg_view('elgg_d3js/circle_packing',array('dataurl' => $dataurl));
 		break;
+	
 	case 'd3js_scatter' :
 		$data_url .= 'data.tsv';
 		$content = elgg_view('elgg_d3js/scatter_plot',array('dataurl' => $data_url));
 		break;
+	
 	case 'd3js_line' :
 		$data_url .= 'data2.tsv';
 		$content = elgg_view('elgg_d3js/line_chart',array('dataurl' => $data_url));
 		break;
+	
 	case 'd3js_sdg' :
 		$data_url .= 'SDGdata.js';
 		$content = elgg_view('elgg_d3js/stack_density_graph',array('dataurl' => $data_url));
 		break;
+	
 	case 'd3js_radar' :
 			$data = array();
 		/*
@@ -65,21 +73,26 @@ switch($viztype)
 
 		//Legend
 		$description = "Nombre de membres par groupe :";
-		$content = elgg_view('elgg_d3js/radar_chart',array('data' => $data,'objects' => $objects,'description' => $description, 'size' => 0.5));
-		$content .= elgg_view('elgg_d3js/radar_chart',array('data' => $data,'objects' => $objects,'description' => $description, 'size' => 0.5));
+		$content = elgg_view('elgg_d3js/radar_chart', array('data' => $data,'objects' => $objects,'description' => $description, 'size' => 0.5));
+		$content .= elgg_view('elgg_d3js/radar_chart', array('data' => $data,'objects' => $objects,'description' => $description, 'size' => 0.5));
 		break;
+	
 	case 'd3js_pie' :
-		$content = elgg_view('elgg_d3js/pie_chart',array('dataurl' => $dataurl));
-		$content .= elgg_view('elgg_d3js/pie_chart',array('dataurl' => $dataurl));
+		$content = elgg_view('elgg_d3js/pie_chart', array('dataurl' => $dataurl));
+		$content .= elgg_view('elgg_d3js/pie_chart', array('dataurl' => $dataurl));
 		break;
+	
 	default :
 		register_error("Aucune visualisation choisie");
 		forward('d3js');
 }
 
+
+// Compose page content
 $body = elgg_view_layout('one_column', array(
-	'content' => $content,
+	'content' => '<div id="elgg-d3js-wrapper"><div class="d3js-' . $viztype . '">' . $content . '</div></div>',
 	'title' => $title,
 ));
 
 echo elgg_view_page($title, $body);
+
